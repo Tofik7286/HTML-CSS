9bf62ea7d1ef11fc1c44907d5bfa7b535960e117
tofik <tofik.imscit21@gmail.com>
2025-01-24 22:56:38 +0530

    Project 21  Added

---
+++ Project 21 Attractive Landing Page/script.js
@@ -0,0 +1,13 @@
+let icon = document.querySelector('.icon');
+let ul = document.querySelector('ul');
+
+icon.addEventListener("click",()=>{
+    ul.classList.toggle("show");
+
+    if (ul.className == "show") {
+        document.getElementById("bar").className="fa-solid fa-xmark";
+    }
+    else{
+        document.getElementById("bar").className="fa-solid fa-bars";
+    }
+})
